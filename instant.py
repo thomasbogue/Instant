6e7bf0d2e573d43a1bf93a0607f5b653e5f5d583
Tom Bogue <thomasbogue@gmail.com>
2023-08-06 11:32:14 -0400

    changed username to display server nickname (if different)

--- instant.py
+++ instant.py
@@ -6,6 +6,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+special_rolls = {1: "GM Intrusion", 17: "Damage Bonus +1", 18: "Damage Bonus +2", 19: "Minor Effect or +3 damage", 20: "Major effect or +4 damage"}
+
 @client.event
 async def on_ready():
   print(f"we've logged on as {client.user}")
@@ -26,7 +28,7 @@ async def on_message(message):
       await rollD(message)
 
 def greeting(message):
-  return message.channel.send(f"Hello {message.author}, I'm Instant Servant, a Cypher RPG die rolling bot.  Type $help for more details on how I can help")
+  return message.channel.send(f"Hello {message.author.display_name}, I'm Instant Servant, a Cypher RPG die rolling bot.  Type $help for more details on how I can help")
 
 def help(message):
   return message.channel.send("Instant Servant:\n  $help\n    show this help\n  $r 3\n    make a level 3 roll\n  $d10 + 3\n    rolls a d10 and adds 3\n  $h +3\n    make a healing roll and add 3\n  $hello\n    Say hi.  Everybody appreciates politeness\nYou can also type $roll instead of $r, or $heal instead of $h -- I'm flexible.\nAnd you choose to skip spaces after + signs, or the + sign at all")
@@ -50,7 +52,10 @@ def roll(message):
       success_message = "Success!"
   else:
       success_message = "Failed."
-  return message.channel.send(f"{success_message}  {message.author} rolled a {roll}, and the target was {target}")
+  m = success_message
+  if roll in special_rolls.keys():
+      m = f"{m}\n{special_rolls[roll]}."
+  return message.channel.send(f"{message.author.display_name} rolled a {roll}, and the target was {target}\n{m}")
 
 def parseBonus(params):
   bonus = 0
@@ -69,9 +74,9 @@ def heal(message):
     bonus = parseBonus(params[1:])
   roll = random.randint(1,6)
   if (bonus == 0):
-    reply = f"{message.author} rolled {roll} on their heal roll"
+    reply = f"{message.author.display_name} rolled {roll} on their heal roll"
   else:
-    reply = f"{message.author} rolled {roll} + {bonus} = {roll + bonus} on their heal roll"
+    reply = f"{message.author.display_name} rolled {roll} + {bonus} = {roll + bonus} on their heal roll"
   return message.channel.send(reply)
 
 def rollD(message):
@@ -85,9 +90,9 @@ def rollD(message):
     bonus = parseBonus(params[1:])
   roll = random.randint(1, d)
   if bonus == 0:
-    reply = f"{message.author} rolled {roll} on 1d{d}"
+    reply = f"{message.author.display_name} rolled {roll} on 1d{d}"
   else:
-    reply = f"{message.author} rolled {roll} + {bonus} = {roll + bonus} on 1d{d} + {bonus}"
+    reply = f"{message.author.display_name} rolled {roll} + {bonus} = {roll + bonus} on 1d{d} + {bonus}"
   return message.channel.send(reply)
 
 client.run(token)
